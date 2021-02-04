d6c25860a9c6f2930348c30b6401bfa2f040e658
Baptisteprvt <72817078+Baptisteprvt@users.noreply.github.com>
2021-02-04 15:28:55 +0100

    Premier commit git2

---
+++ ProjetS2021/Assets/Sound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("Musicvol", Mathf.Log10(sliderValue)*20);
+    }
+
+    /*
+     void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 70, 150, 30), "Pause"))
+        {
+            audioData.Pause();
+            Debug.Log("Pause: " + audioData.time);
+        }
+
+        if (GUI.Button(new Rect(10, 170, 150, 30), "Continue"))
+        {
+            audioData.UnPause();
+        }
+    }
+    */
+}
